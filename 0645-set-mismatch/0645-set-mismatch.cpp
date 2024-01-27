3eac801cf16a0290fbf13b6f148f2f49510e351f
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-01-27 14:16:46 +0530

    Time: 15 ms (93.45%), Space: 23.7 MB (93.72%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i++) {
+            while (nums[i] != i + 1 && nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != i + 1) {
+                ans.push_back(nums[i]);  // Duplicate number
+                ans.push_back(i + 1);    // Missing number
+                break;
+            }
+        }
+        
+        return ans;
+    }
+};

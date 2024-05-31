d3152fae1fd4d4f5b7ac3808f4527a5cb530ce54
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-05-31 10:26:35 +0530

    Time: 10 ms (38.25%), Space: 13.9 MB (22.79%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -3,12 +3,13 @@ public:
     vector<int> singleNumber(vector<int>& nums) {
         unordered_map<int,int>mp;
         vector<int>ans;
-        for(auto ele:nums){
-            mp[ele]++;
+        for(int i=0;i<nums.size();i++){
+            if(mp.find(nums[i])!=mp.end()){
+                mp.erase(nums[i]);
+            }else mp[nums[i]]++;
         }
-
-        for(auto ele: mp ){
-            if(ele.second==1)ans.push_back(ele.first);
+        for(auto ele :mp){
+            ans.push_back(ele.first);
         }
 
         return ans;

d4363b8112ff5bd810d6756a038a7447dc016e76
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-06-01 10:02:22 +0530

    Time: 0 ms (100%), Space: 7.8 MB (84.36%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,25 +2,14 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char,int>mp;
-        for(auto ele:s){
-            mp[ele]++;
-        }
-        char ans;
+      int res=0;
+      for(int i=0;i<s.size();i++){
+        res^=s[i];
+      }
+      for(int i=0;i<t.size();i++){
+        res^=t[i];
+      }
 
-        for(int i=0;i<t.length();i++){
-            if(mp.find(t[i])!=mp.end()){
-                mp[t[i]]--;
-                if (mp[t[i]] == 0) {
-                    mp.erase(t[i]);
-                }
-
-            }
-            else {
-                ans=t[i];
-                break;
-            }  
-        }
-        return ans;
+      return (char)res;
     }
 };

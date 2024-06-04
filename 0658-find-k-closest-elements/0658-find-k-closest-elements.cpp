9384df19916ca5c0b8459d23be005c75d26186fa
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-06-05 02:03:46 +0530

    Time: 63 ms (9.78%), Space: 37.8 MB (18.27%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,80 +1,23 @@
-class Solution
-{
+class Solution {
 public:
-    vector<int> findClosestElements(vector<int> &arr, int k, int x)
-    {
-        int n = arr.size();
-        vector<int> v(k);
-        
-        // case 1
-        if (x <= arr[0])
-        {
-            for (int i = 0; i < k; i++)
-            {
-                v[i] = arr[i];
-            }
-            return v;
-        }
-        
-        // case 2
-        if (x >= arr[n - 1])
-        {
-            int i = n - 1;
-            int j = k - 1;
-            while (j >= 0)
-            {
-                v[j] = arr[i];
-                j--;
-                i--;
-            }
-            return v;
-        }
+    typedef pair<int,int>pi;
+    typedef vector<int>vet;
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pi>pq;
 
-        // Binary search to find closest index to x
-        int low = 0;
-        int high = n - 1;
-        while (low <= high)
-        {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] == x)
-            {
-                low = mid;
-                break;
-            }
-            else if (arr[mid] > x)
-            {
-                high = mid - 1;
-            }
-            else
-            {
-                low = mid + 1;
-            }
+        for(int ele:arr){
+            int dis=abs(x-ele);
+            pq.push({dis,ele});
+            if(pq.size()>k)pq.pop();
         }
-        
-        int lb = low - 1;
-        int up = low;
-        
-        while (k--)
-        {
-            if (lb < 0)
-            {
-                v[k] = arr[up++];
-            }
-            else if (up == n)
-            {
-                v[k] = arr[lb--];
-            }
-            else if (abs(arr[lb] - x) <= abs(arr[up] - x))
-            {
-                v[k] = arr[lb--];
-            }
-            else
-            {
-                v[k] = arr[up++];
-            }
+        vet ans;
+        while(!pq.empty()){
+            int ele=pq.top().second;
+            ans.push_back(ele);
+            pq.pop();
         }
-        
-        sort(v.begin(), v.end());
-        return v;
+        sort(ans.begin(),ans.end());
+        return ans;
+
     }
 };

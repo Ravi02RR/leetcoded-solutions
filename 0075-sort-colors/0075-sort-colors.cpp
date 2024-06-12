569ff59dc3b4e3714eb1ee6f9e6f09c690bd9bcc
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-06-12 16:24:28 +0530

    Time: 0 ms (100%), Space: 9.8 MB (34.14%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int zidx = 0, twoIdx = nums.size() - 1, i = 0;
-        while (i <= twoIdx) {
-            if (nums[i] == 0) {
-                swap(nums[i], nums[zidx]);
-                zidx++;
-                i++;
-            } else if (nums[i] == 2) {
-                swap(nums[i], nums[twoIdx]);
-                twoIdx--;
-            } else {
-                i++;
-            }
+        int zcount=0;
+        int oCount=0;
+        int tcount=0;
+
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0)zcount++;
+            else if(nums[i]==1)oCount++;
+            else tcount++;
+
         }
+        for(int i=0;i<nums.size();i++){
+           if(i<zcount) nums[i]=0;
+           else if(i<(zcount+oCount)) nums[i]=1;
+           else nums[i]=2;
+       }
+
+
     }
 };

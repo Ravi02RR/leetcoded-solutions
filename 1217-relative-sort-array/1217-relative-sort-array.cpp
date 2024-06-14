80ce7b41364b4c46772719bf98023968dbddb35f
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-06-14 16:31:05 +0530

    Time: 5 ms (32.53%), Space: 10.1 MB (50.37%) - LeetHub

---
+++ 1217-relative-sort-array/1217-relative-sort-array.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+       vector<int>ans;
+        map<int,int>mp;
+
+        for(auto ele : arr1){
+            mp[ele]++;
+        }
+
+        for(int i=0;i<arr2.size();i++){
+            int freq=mp[arr2[i]];
+            while(freq>=1){
+                ans.push_back(arr2[i]);
+                freq--;
+                
+            }
+            mp.erase(arr2[i]);
+        }
+
+        for(auto ele :mp){
+           int frq=ele.second;
+           while(frq>=1){
+             ans.push_back(ele.first);
+             frq--;
+           }
+        }
+
+        
+
+
+        return ans;
+
+    }
+};

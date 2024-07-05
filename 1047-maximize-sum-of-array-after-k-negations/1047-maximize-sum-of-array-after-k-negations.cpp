93e9657116e18994b63ed0be7adf613940ef411e
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-07-06 00:33:46 +0530

    Time: 3 ms (77.58%), Space: 12.6 MB (30.67%) - LeetHub

--- 1047-maximize-sum-of-array-after-k-negations/1047-maximize-sum-of-array-after-k-negations.cpp
+++ 1047-maximize-sum-of-array-after-k-negations/1047-maximize-sum-of-array-after-k-negations.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int largestSumAfterKNegations(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>>pq(nums.begin(),nums.end());
-        int sum=0;
-        for(int i=0;i<nums.size();i++)sum+=nums[i];
-        while(k--){
-            if(pq.top()==0)break;
-            int ele=pq.top();
-            pq.pop();
-            sum-=ele;
-            pq.push(ele*-1);
-            sum+=(-ele);
-        }
+     priority_queue<int,vector<int>,greater<int>>pq(nums.begin(),nums.end());
+     int sum=0;
+     for(int i=0;i<nums.size();i++)sum+=nums[i];
+     while(k--){
+        if(pq.top()==0)break;
+        int ele=pq.top();
+        pq.pop();
+        sum-=ele;
+        pq.push(-1*ele);
+        sum+=(-1*ele);
+        
 
-        return sum;
+
+     }
+     return sum;
     }
 };

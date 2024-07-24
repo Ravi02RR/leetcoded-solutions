c880192a0229a940d77993f2d55a2461d612f340
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-07-24 12:01:01 +0530

    Time: 23 ms (61.34%), Space: 22.9 MB (35.09%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,23 +1,31 @@
 class Solution {
+private:
+    static bool cmp(vector<int>& a, vector<int>& b) {
+        // Sort based on start time
+        return a[0] < b[0];
+    }
+
 public:
-    vector<vector<int>> merge(vector<vector<int>>& v) {
-        vector<vector<int>>ans;
-        sort(v.begin(),v.end());
-
-        ans.push_back(v[0]);
-        for(int i=1;i<v.size();i++){
-            if(v[i][0]<=ans.back()[1]){
-                ans.back()[1]=max(v[i][1],ans.back()[1]);
-            }
-            else{
-                ans.push_back(v[i]);
-            }
-          
-            
-
-        }
-        return ans;
-
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if (intervals.empty()) return {};
         
+        sort(intervals.begin(), intervals.end(), cmp);
+
+        vector<vector<int>> res;
+        res.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); i++) {
+            vector<int>& last = res.back();
+            vector<int>& curr = intervals[i];
+
+            if (curr[0] <= last[1]) { // overlapping
+                
+                last[1] = max(last[1], curr[1]);
+            } else { // not overlapping
+                res.push_back(curr);
+            }
+        }
+
+        return res;
     }
 };

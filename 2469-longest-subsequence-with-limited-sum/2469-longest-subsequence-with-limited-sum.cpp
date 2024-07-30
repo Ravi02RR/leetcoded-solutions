fe1bb1e4f9dd317a9d323d6f98e7fb4e14ed58e6
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-07-30 22:50:15 +0530

    Time: 12 ms (73.73%), Space: 16.4 MB (37.48%) - LeetHub

--- 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.cpp
+++ 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+    int binary(int n, vector<int>& nums) {
+        int ans = -1; 
+
+        int start = 0;
+        int end = nums.size() - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] <= n) {
+                ans = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return ans + 1;  
+    }
+
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
         int n = nums.size();
         int m = queries.size();
@@ -14,12 +32,7 @@ public:
 
         for (int i = 0; i < m; i++) {
             int val = queries[i];
-            int temp = 0;
-            for (int j = 0; j < n; j++) {
-                if (nums[j] > val) break;
-                temp++;
-            }
-            ans[i] = temp;
+            ans[i] = binary(val, nums);
         }
 
         return ans;

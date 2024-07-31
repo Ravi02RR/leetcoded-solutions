3db6c364efb2918edaf73a24e5f911323c7e475e
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-07-31 23:56:10 +0530

    Time: 2 ms (72.5%), Space: 11.5 MB (15.26%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        ios_base::sync_with_stdio(false);
+        std::ios_base::sync_with_stdio(false);
+       
         sort(strs.begin(),strs.end());
 
         for(int i=0;i<strs.size();i++){

fd7be69f24e057195b3e0b075c7eed69ec0c2885
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-08-01 15:24:02 +0530

    Time: 4 ms (82.75%), Space: 17.7 MB (16.71%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -3,12 +3,12 @@ public:
     int countSeniors(vector<string>& s) {
         int ans=0;
         for(int i=0;i<s.size();i++){
-            char one=s[i][11];
-            char two=s[i][12];
+            char one=s[i].at(11);
+            char two=s[i].at(12);
             int temp=(one-'0')*10+two-'0';
             if(temp>60)ans++;
             
-         }
+        }
          return ans;
 
         

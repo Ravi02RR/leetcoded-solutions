8f997a01ee1bba151b1be61e841603c92426ed5a
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-08-02 17:38:30 +0530

    Time: 23 ms (63.09%), Space: 34.5 MB (10.58%) - LeetHub

--- 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.cpp
@@ -1,42 +1,58 @@
 class Solution {
 public:
-    int maxSatisfied(vector<int>& cus, vector<int>& g, int k) {
+    int maxloss(vector<int>& customers, vector<int>& grumpy, int minutes) {
+        int idx = 0;
+        int max_loss = 0;
+        int current_loss = 0;
 
-        int n=cus.size();
-
-        int prevLoss=0;
-        for(int i=0;i<k;i++){
-            if(g[i]==1)prevLoss+=cus[i];
+        // Initial loss calculation for the first 'minutes' interval
+        for (int i = 0; i < minutes; i++) {
+            current_loss += (grumpy[i] == 1) ? customers[i] : 0;
         }
-        int maxLoss=prevLoss;
-        int idx=0;
+        max_loss = current_loss;
 
-        int i=1;
-        int j=k;
-        while(j<n){ 
-           
-            if(g[j]==1)prevLoss+=cus[j];
-            if(g[i-1]==1)prevLoss-=cus[i-1];
-            if(maxLoss<prevLoss){
-                maxLoss=prevLoss;
-                idx=i;
+        int n = customers.size();
+        int i = 1;
+        int j = minutes;
+
+        // Sliding window to find the maximum loss interval
+        while (j < n) {
+            current_loss += (grumpy[j] == 1) ? customers[j] : 0;
+            current_loss -= (grumpy[i - 1] == 1) ? customers[i - 1] : 0;
+
+            if (current_loss > max_loss) {
+                max_loss = current_loss;
+                idx = i;
             }
             i++;
             j++;
         }
+        return idx;
+    }
 
-        for(int i=idx;i<idx+k;i++){
-            g[i]=0;
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+        int ans = 0;
+
+        // Calculate the total satisfaction without any change
+        for (int i = 0; i < customers.size(); i++) {
+            if (grumpy[i] == 0) {
+                ans += customers[i];
+            }
         }
 
-        int sum=0;
-        for(int i=0;i<n;i++){
-            if(g[i]!=1)sum+=cus[i];
+        // Find the interval with the maximum loss
+        int idx = maxloss(customers, grumpy, minutes);
+        cout << "index " << idx << endl;
+
+        // Calculate the additional satisfaction by flipping the grumpy values in the identified interval
+        for (int i = idx; i < idx + minutes; i++) {
+            if (grumpy[i] == 1) {
+                ans += customers[i];
+            }
         }
 
-
-        return sum;
-
-        
+        return ans;
     }
 };
+
+

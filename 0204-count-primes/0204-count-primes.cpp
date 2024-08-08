cac468e4d24feb1a792c8b716ea79c6126c44881
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-08-08 12:15:35 +0530

    Time: 445 ms (42.79%), Space: 11.8 MB (57.22%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -8,7 +8,7 @@ public:
         vector<bool> is_prime(n, true);
         is_prime[0] = is_prime[1] = false;
 
-        for (int i = 2; i * i < n; i++) {
+        for (int i = 2; i < sqrt(n); i++) {
             if (is_prime[i]) {
                 for (int j = i * i; j < n; j += i) {
                     is_prime[j] = false;
@@ -17,7 +17,7 @@ public:
         }
 
         int count = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 2; i < n; i++) {
             if (is_prime[i]) {
                 count++;
             }

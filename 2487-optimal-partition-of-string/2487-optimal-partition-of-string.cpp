eb4ab38b6f2cbedf91fc64ef7e3cf120e28bc849
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-08-27 13:57:44 +0530

    Time: 162 ms (27.6%), Space: 47.3 MB (35.05%) - LeetHub

---
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int partitionString(string s) {
+        int count=0;
+        unordered_set<char>st;
+        for(int i=0;i<s.size();i++){
+        
+            if(st.find(s[i])==st.end())st.insert(s[i]);
+            else {
+                count++;
+                st.clear();
+                st.insert(s[i]);
+            }
+        }
+        return count+1;
+        
+    }
+};

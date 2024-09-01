50a8544ff4b7a3237fd925a5b1b3f37cb91453ef
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-09-01 21:38:24 +0530

    Time: 94 ms (15.95%), Space: 97.7 MB (12.97%) - LeetHub

---
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    // void helper(vector<int>& original, int m, int n, int idx, vector<int> temp ) {
+    //     if (idx == original.size()) {
+    //         if (!temp.empty()) {  
+    //             ans.push_back(temp);
+    //         }
+    //         return;
+    //     }
+        
+    //     if (temp.size() == n) { 
+    //         ans.push_back(temp);
+    //         temp.clear();  
+    //     }
+        
+    //     temp.push_back(original[idx]); 
+    //     helper(original, m, n, idx + 1, temp); 
+    // }
+
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        if (original.size() != m * n) return {};   
+        // vector<int> temp;
+        // helper(original, m, n, 0, temp);
+
+        int temp=0;
+        vector<int>tempArr;
+
+        for(int i=0;i<original.size();i++){
+            tempArr.push_back(original[i]);
+            temp++;
+            if(temp==n){
+                ans.push_back(tempArr);
+                tempArr.clear();
+                temp=0;
+            }
+
+        }
+
+
+        return ans;
+    }
+};
